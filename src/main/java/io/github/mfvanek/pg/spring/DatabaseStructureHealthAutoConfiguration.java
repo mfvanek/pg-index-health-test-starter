caf52a7507181ead4233facab4590a33c9135b16
Ivan Vakhrushev <IVakhrushev@innotechnum.com>
2021-05-22 20:51:36 +0300

    Added copyright

--- src/main/java/io/github/mfvanek/pg/spring/DatabaseStructureHealthAutoConfiguration.java
+++ src/main/java/io/github/mfvanek/pg/spring/DatabaseStructureHealthAutoConfiguration.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2021-2021. Ivan Vakhrushev.
+ * https://github.com/mfvanek/pg-index-health-test-starter
+ *
+ * This file is a part of "pg-index-health-test-starter".
+ *
+ * Licensed under the Apache License 2.0
+ */
+
 package io.github.mfvanek.pg.spring;
 
 import io.github.mfvanek.pg.connection.PgConnection;
@@ -22,6 +31,7 @@ import javax.sql.DataSource;
 @AutoConfigureAfter({DataSourceAutoConfiguration.class})
 public class DatabaseStructureHealthAutoConfiguration {
 
+  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
   @Bean
   @ConditionalOnBean(name = "dataSource")
   @ConditionalOnMissingBean
