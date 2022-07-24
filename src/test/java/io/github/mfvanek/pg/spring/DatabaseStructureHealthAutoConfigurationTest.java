b2471383514627cfa61f9cb57cf9b42a474d3be0
Ivan Vakhrushev <i.vakhrushev@banki.ru>
2022-07-24 12:23:43 +0300

    100% mutation coverage

--- src/test/java/io/github/mfvanek/pg/spring/DatabaseStructureHealthAutoConfigurationTest.java
+++ src/test/java/io/github/mfvanek/pg/spring/DatabaseStructureHealthAutoConfigurationTest.java
@@ -9,67 +9,11 @@
 
 package io.github.mfvanek.pg.spring;
 
-import io.github.mfvanek.pg.checks.host.ColumnsWithoutDescriptionCheckOnHost;
-import io.github.mfvanek.pg.checks.host.DuplicatedIndexesCheckOnHost;
-import io.github.mfvanek.pg.checks.host.ForeignKeysNotCoveredWithIndexCheckOnHost;
-import io.github.mfvanek.pg.checks.host.IndexesWithBloatCheckOnHost;
-import io.github.mfvanek.pg.checks.host.IndexesWithNullValuesCheckOnHost;
-import io.github.mfvanek.pg.checks.host.IntersectedIndexesCheckOnHost;
-import io.github.mfvanek.pg.checks.host.InvalidIndexesCheckOnHost;
-import io.github.mfvanek.pg.checks.host.TablesWithBloatCheckOnHost;
-import io.github.mfvanek.pg.checks.host.TablesWithMissingIndexesCheckOnHost;
-import io.github.mfvanek.pg.checks.host.TablesWithoutDescriptionCheckOnHost;
-import io.github.mfvanek.pg.checks.host.TablesWithoutPrimaryKeyCheckOnHost;
-import io.github.mfvanek.pg.checks.host.UnusedIndexesCheckOnHost;
-import io.github.mfvanek.pg.connection.PgConnection;
-import io.github.mfvanek.pg.settings.maintenance.ConfigurationMaintenanceOnHost;
-import io.github.mfvanek.pg.statistics.maintenance.StatisticsMaintenanceOnHost;
-import org.apache.commons.text.WordUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.FilteredClassLoader;
-import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Predicate;
-import javax.annotation.Nonnull;
-import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
-class DatabaseStructureHealthAutoConfigurationTest {
-
-    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner();
-
-    private final Predicate<String> beanNamesFilter = b -> !b.startsWith("org.springframework") && !b.startsWith("pg.index.health.test") &&
-        !b.endsWith("AutoConfiguration") && !"dataSource".equals(b);
-
-    private static final List<String> EXPECTED_BEANS = Collections.unmodifiableList(Arrays.asList(
-        "pgConnection",
-        "duplicatedIndexesCheckOnHost",
-        "foreignKeysNotCoveredWithIndexCheckOnHost",
-        "indexesWithBloatCheckOnHost",
-        "indexesWithNullValuesCheckOnHost",
-        "intersectedIndexesCheckOnHost",
-        "invalidIndexesCheckOnHost",
-        "tablesWithBloatCheckOnHost",
-        "tablesWithMissingIndexesCheckOnHost",
-        "tablesWithoutPrimaryKeyCheckOnHost",
-        "unusedIndexesCheckOnHost",
-        "tablesWithoutDescriptionCheckOnHost",
-        "columnsWithoutDescriptionCheckOnHost",
-        "statisticsMaintenanceOnHost",
-        "configurationMaintenanceOnHost"
-    ));
+class DatabaseStructureHealthAutoConfigurationTest extends AutoConfigurationTestBase {
 
     @Test
     void withoutDataSource() {
@@ -83,16 +27,15 @@ class DatabaseStructureHealthAutoConfigurationTest {
     @Test
     void withDataSource() {
         assertWithTestConfig()
-            .withInitializer(DatabaseStructureHealthAutoConfigurationTest::initialize)
+            .withInitializer(AutoConfigurationTestBase::initialize)
             .run(context -> {
                 assertThat(context.getBeanDefinitionNames())
                     .filteredOn(beanNamesFilter)
-                    .hasSize(15)
+                    .hasSameSizeAs(EXPECTED_BEANS)
                     .containsAll(EXPECTED_BEANS);
                 EXPECTED_BEANS.forEach(n ->
                     assertThat(context.getBean(n))
-                        .isNotNull()
-                );
+                        .isInstanceOfAny(EXPECTED_TYPES));
             });
     }
 
@@ -100,7 +43,7 @@ class DatabaseStructureHealthAutoConfigurationTest {
     void shouldNotCreateAutoConfigurationWithDisabledProperty() {
         assertWithTestConfig()
             .withPropertyValues("pg.index.health.test.enabled=false")
-            .withInitializer(DatabaseStructureHealthAutoConfigurationTest::initialize)
+            .withInitializer(AutoConfigurationTestBase::initialize)
             .run(context -> assertThat(context.getBeanDefinitionNames())
                 .isNotEmpty()
                 .filteredOn(beanNamesFilter)
@@ -111,73 +54,16 @@ class DatabaseStructureHealthAutoConfigurationTest {
     void shouldCreateAutoConfigurationWhenPropertyExplicitlySet() {
         assertWithTestConfig()
             .withPropertyValues("pg.index.health.test.enabled=true")
-            .withInitializer(DatabaseStructureHealthAutoConfigurationTest::initialize)
-            .run(context -> assertThat(context.getBeanDefinitionNames())
-                .isNotEmpty()
-                .filteredOn(beanNamesFilter)
-                .hasSize(15));
-    }
-
-    @Test
-    void withoutPgConnectionClass() {
-        assertThatCode(() -> AccessController.doPrivileged((PrivilegedAction<?>) () -> {
-            assertWithTestConfig()
-                .withInitializer(DatabaseStructureHealthAutoConfigurationTest::initialize)
-                .withClassLoader(new FilteredClassLoader(PgConnection.class))
-                .run(context -> assertThat(context.getBeanDefinitionNames())
+            .withInitializer(AutoConfigurationTestBase::initialize)
+            .run(context -> {
+                assertThat(context.getBeanDefinitionNames())
                     .isNotEmpty()
                     .filteredOn(beanNamesFilter)
-                    .isEmpty());
-            return null;
-        })).doesNotThrowAnyException();
-    }
-
-    @ParameterizedTest
-    @ValueSource(classes = {
-        DuplicatedIndexesCheckOnHost.class,
-        ForeignKeysNotCoveredWithIndexCheckOnHost.class,
-        IndexesWithBloatCheckOnHost.class,
-        IndexesWithNullValuesCheckOnHost.class,
-        IntersectedIndexesCheckOnHost.class,
-        InvalidIndexesCheckOnHost.class,
-        TablesWithBloatCheckOnHost.class,
-        TablesWithMissingIndexesCheckOnHost.class,
-        TablesWithoutPrimaryKeyCheckOnHost.class,
-        UnusedIndexesCheckOnHost.class,
-        TablesWithoutDescriptionCheckOnHost.class,
-        ColumnsWithoutDescriptionCheckOnHost.class,
-        StatisticsMaintenanceOnHost.class,
-        ConfigurationMaintenanceOnHost.class})
-    void withoutClass(final Class<?> type) {
-        AccessController.doPrivileged((PrivilegedAction<?>) () -> {
-            assertWithTestConfig()
-                .withInitializer(DatabaseStructureHealthAutoConfigurationTest::initialize)
-                .withClassLoader(new FilteredClassLoader(type))
-                .run(context -> assertThat(context)
-                    .hasBean("pgConnection")
-                    .doesNotHaveBean(getBeanName(type))
-                    .satisfies(c -> assertThat(c.getBeanDefinitionNames())
-                        .isNotEmpty()
-                        .filteredOn(beanNamesFilter)
-                        .hasSize(14)
-                        .allSatisfy(n -> assertThat(context.getBean(n))
-                            .isNotNull())));
-            return null;
-        });
-    }
-
-    @Nonnull
-    private ApplicationContextRunner assertWithTestConfig() {
-        return contextRunner.withUserConfiguration(DatabaseStructureHealthAutoConfiguration.class);
-    }
-
-    private static <C extends ConfigurableApplicationContext> void initialize(@Nonnull final C applicationContext) {
-        final GenericApplicationContext context = (GenericApplicationContext) applicationContext;
-        context.registerBean("dataSource", DataSource.class, () -> Mockito.mock(DataSource.class));
-    }
-
-    @Nonnull
-    private static String getBeanName(@Nonnull final Class<?> type) {
-        return WordUtils.uncapitalize(type.getSimpleName());
+                    .hasSameSizeAs(EXPECTED_BEANS)
+                    .containsAll(EXPECTED_BEANS);
+                EXPECTED_BEANS.forEach(n ->
+                    assertThat(context.getBean(n))
+                        .isInstanceOfAny(EXPECTED_TYPES));
+            });
     }
 }
