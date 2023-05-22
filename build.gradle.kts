100d0c42259f5d36340c154efbe9cd7e06a26892
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2023-05-22 15:34:04 +0400

    Migration to Kotlin DSL (#66)
    * Preparing for migration to Kotlin DSL
    
    * Migrating to Kotlin DSL
    
    * Migrating to Kotlin DSL
    
    ---------
    
    Co-authored-by: i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>

---
+++ build.gradle.kts
@@ -0,0 +1,250 @@
+import com.github.spotbugs.snom.Confidence
+import com.github.spotbugs.snom.Effort
+import com.github.spotbugs.snom.SpotBugsTask
+import info.solidsoft.gradle.pitest.PitestTask
+import net.ltgt.gradle.errorprone.errorprone
+import org.sonarqube.gradle.SonarTask
+
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("signing")
+    id("jacoco")
+    id("com.github.spotbugs") version "5.0.14"
+    id("checkstyle")
+    id("pmd")
+    id("org.sonarqube") version "4.0.0.2929"
+    id("info.solidsoft.pitest") version "1.9.11"
+    id("net.ltgt.errorprone") version "3.1.0"
+}
+
+group = "io.github.mfvanek"
+version = "0.9.2-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+val springVersion = "2.7.12"
+
+dependencies {
+    api("io.github.mfvanek:pg-index-health:0.9.1")
+    implementation("org.springframework.boot:spring-boot-starter:$springVersion")
+    annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor:$springVersion")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:$springVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springVersion")
+    testImplementation(enforcedPlatform("org.junit:junit-bom:5.9.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.apache.commons:commons-text:1.10.0")
+    testImplementation("com.google.code.findbugs:jsr305:3.0.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+
+    pitest("it.mulders.stryker:pit-dashboard-reporter:0.1.5")
+    checkstyle("com.thomasjensen.checkstyle.addons:checkstyle-addons:7.0.1")
+    errorprone("com.google.errorprone:error_prone_core:2.19.1")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withJavadocJar()
+    withSourcesJar()
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.errorprone.disableWarningsInGeneratedCode.set(true)
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        dependsOn(checkstyleMain, checkstyleTest, pmdMain, pmdTest, spotbugsMain, spotbugsTest)
+        finalizedBy(jacocoTestReport)
+        finalizedBy(jacocoTestCoverageVerification)
+    }
+
+    jar {
+        manifest {
+            attributes["Implementation-Title"] = project.name
+            attributes["Implementation-Version"] = project.version
+        }
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
+
+    javadoc {
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+
+    jacocoTestReport {
+        dependsOn(test)
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+    }
+
+    jacocoTestCoverageVerification {
+        dependsOn(test)
+        violationRules {
+            rule {
+                limit {
+                    counter = "CLASS"
+                    value = "MISSEDCOUNT"
+                    maximum = "0.0".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "METHOD"
+                    value = "MISSEDCOUNT"
+                    maximum = "0.0".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "LINE"
+                    value = "MISSEDCOUNT"
+                    maximum = "0.0".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "INSTRUCTION"
+                    value = "COVEREDRATIO"
+                    minimum = "1.0".toBigDecimal()
+                }
+            }
+        }
+    }
+
+    check {
+        dependsOn(jacocoTestReport, jacocoTestCoverageVerification)
+    }
+}
+
+checkstyle {
+    toolVersion = "10.7.0"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+    maxWarnings = 0
+    maxErrors = 0
+}
+
+pmd {
+    isConsoleOutput = true
+    toolVersion = "6.54.0"
+    ruleSetFiles = files("config/pmd/pmd.xml")
+    ruleSets = listOf()
+}
+
+spotbugs {
+    showProgress.set(true)
+    effort.set(Effort.MAX)
+    reportLevel.set(Confidence.LOW)
+    excludeFilter.set(file("config/spotbugs/exclude.xml"))
+}
+tasks.withType<SpotBugsTask>().configureEach {
+    reports {
+        create("xml") { enabled = true }
+        create("html") { enabled = true }
+    }
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "mfvanek_pg-index-health-test-starter")
+        property("sonar.organization", "mfvanek")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+tasks.withType<SonarTask>().configureEach {
+    dependsOn(tasks.test, tasks.jacocoTestReport)
+}
+
+pitest {
+    setProperty("junit5PluginVersion", "1.1.2")
+    setProperty("pitestVersion", "1.10.4")
+    threads.set(4)
+    if (System.getenv("STRYKER_DASHBOARD_API_KEY") != null) {
+        outputFormats.set(setOf("stryker-dashboard"))
+    } else {
+        outputFormats.set(setOf("HTML"))
+    }
+    timestampedReports.set(false)
+    mutationThreshold.set(100)
+}
+tasks.withType<PitestTask>().configureEach {
+    mustRunAfter(tasks.test)
+}
+tasks.build {
+    dependsOn("pitest")
+}
+
+signing {
+    if (!version.toString().endsWith("SNAPSHOT")) {
+        useGpgCmd()
+        sign(publishing.publications["mavenJava"])
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+
+            pom {
+                name.set(project.name)
+                description.set("Spring Boot Starter for pg-index-health library")
+                url.set("https://github.com/mfvanek/pg-index-health-test-starter")
+                licenses {
+                    license {
+                        name.set("Apache License Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("mfvanek")
+                        name.set("Ivan Vakhrushev")
+                        email.set("mfvanek@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/mfvanek/pg-index-health-test-starter.git")
+                    developerConnection.set("scm:git@github.com:mfvanek/pg-index-health-test-starter.git")
+                    url.set("https://github.com/mfvanek/pg-index-health-test-starter")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+            val sonatypeUsername: String by project
+            val sonatypePassword: String by project
+            credentials {
+                username = sonatypeUsername
+                password = sonatypePassword
+            }
+        }
+    }
+}
