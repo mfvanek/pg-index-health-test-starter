cb84f94df27e921c3f081f524bfd55c5b3f52d82
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2023-05-30 22:47:41 +0400

    Migrated to Gradle Version Catalog. Updated pitest (#75)
    Co-authored-by: i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "pg-index-health-test-starter"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            val springBoot = version("spring-boot", "2.7.12")
+            library("spring-boot-starter", "org.springframework.boot", "spring-boot-starter")
+                .versionRef(springBoot)
+            library("spring-boot-starterTest", "org.springframework.boot", "spring-boot-starter-test")
+                .versionRef(springBoot)
+            library("spring-boot-autoconfigureProcessor", "org.springframework.boot", "spring-boot-autoconfigure-processor")
+                .versionRef(springBoot)
+            library("spring-boot-configurationProcessor", "org.springframework.boot", "spring-boot-configuration-processor")
+                .versionRef(springBoot)
+        }
+    }
+}
