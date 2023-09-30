5a10840801da86092f70c02356e137756f09f059
Ivan Vakhrushev <mfvanek@gmail.com>
2023-09-30 11:37:13 +0400

    Release v.0.9.5 (#100)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ description = "pg-index-health-test-starter build"
 
 allprojects {
     group = "io.github.mfvanek"
-    version = "0.9.5-SNAPSHOT"
+    version = "0.9.5"
 
     repositories {
         mavenLocal()
