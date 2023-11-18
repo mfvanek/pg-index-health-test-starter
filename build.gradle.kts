67caf3d15ece2e3a2f245d673b706a905c6b73f9
Ivan Vakhrushev <mfvanek@gmail.com>
2023-11-18 16:32:14 +0400

    Next development iteration v0.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ description = "pg-index-health-test-starter build"
 
 allprojects {
     group = "io.github.mfvanek"
-    version = "0.10.1"
+    version = "0.10.2-SNAPSHOT"
 
     repositories {
         mavenLocal()
