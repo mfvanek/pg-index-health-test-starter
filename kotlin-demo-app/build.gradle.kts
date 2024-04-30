5e23f5d131dd13dc297b5a42b17ed01d4a364ddd
Ivan Vakhrushev <mfvanek@gmail.com>
2024-05-01 00:09:30 +0400

    Add Detekt

--- kotlin-demo-app/build.gradle.kts
+++ kotlin-demo-app/build.gradle.kts
@@ -1,3 +1,4 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -6,6 +7,7 @@ plugins {
     id("pg-index-health-test-starter.java-compilation")
     alias(libs.plugins.spring.boot.gradlePlugin)
     alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.detekt)
 }
 
 ext["commons-lang3.version"] = libs.versions.commons.lang3.get()
@@ -22,11 +24,26 @@ dependencies {
 
     testImplementation(libs.spring.boot.starter.test)
     testImplementation(libs.assertj.core)
+
+    detektPlugins(libs.detekt.formatting)
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "11"
+    }
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    config.setFrom(file("../config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
     }
 }
